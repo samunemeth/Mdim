640fc1cbfe27e0072659120e1c84df55f78c7120
samunemeth <76793122+samunemeth@users.noreply.github.com>
2021-06-17 16:39:58 +0200

    ES6 compatibility

--- Mdim.js
+++ Mdim.js
@@ -1,33 +1,11 @@
 /*
     Created by: Samu Németh
+    Creation date: 2021
     License: MIT
     Contact:
         E-mail: samunemeth.hu@gmail.com
         Website: http://www.samunemeth.hu/
 */
-
-/*
-    Copyright 2021 Samu Németh
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-/*
-    TABLE OF CONTENT
-
-    --------Name and number-----------------------function name and parameter----------
-
-    1.0 Multi dimensional arrays
-        1.1 2D array creator                        make2DArray(x, y);
-        1.2 Multi dimensional array creator         makeArray(dim);
-    2.0 Array operations                            
-        2.1 Remove element                          removeElement(arr, elem);
-        2.2 Copy array                              copyArr(arr);
-        2.3 Multidimensional forEach                forEachM(arr, f(e, pos));
-        2.4 Fill multidimensional array             fillM(arr, f(pos, e));
-*/
-
 
 
 /* --- 1.0 Multi dimensional arrays --- */
@@ -39,7 +17,9 @@
 */
 
 function make2DArray(x, y) {
+
     arr = new Array(x);
+    
     for (let i = 0; i < arr.length; i++) {
         arr[i] = new Array(y);
     };
@@ -148,7 +128,7 @@ Array.prototype.fillM = function (f, from = []) {
 
     if (!this.length) return;
     const evaluate = !this[0] || this[0].constructor != Array;
-    const static = f?.constructor != Function;
+    const static = f && f.constructor != Function;
 
     for (let i = 0; i < this.length; i++) {
 
